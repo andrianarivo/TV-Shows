672cf52a5c1575c4ecc7ef5841f8600816c97275
vangelif <ev.fotiades@yahoo.com>
2023-05-23 17:10:50 +0300

    build modal popup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import movies from './modules/TvShowApi.js';
 import './style.scss';
 
+const modal = document.querySelector('section');
+const closeModal = document.getElementById('close-button');
+
 let body = '';
 movies.forEach((movie) => {
-  body += `<div class="card">
+  body += `<div id="${movie.id}" class="card">
         <img src="#" />
         <a>${movie.name}</a>
         <i class="fa-regular fa-heart"></i>
         <p>5 likes</p>
-        <button id=${movie.id} data-target="#popup-wrapper">Comments</button>
+        <button class="button" id=${movie.id} data-target="#popup-wrapper">Comments</button>
       </div>`;
 });
 
 const main = document.querySelector('main');
 main.innerHTML = body;
+
+document.addEventListener('click', (e) => {
+  e.target.className.includes('button');
+  if (e.target.className.includes('button')) {
+    modal.style.display = 'block';
+    console.log('clicked', e.target.id);
+  }
+});
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
