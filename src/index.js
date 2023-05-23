0099d626bd5523cb1d6a395054552db2295c28ed
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-23 15:47:14 +0300

    build fetch movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import movies from './modules/TvShowApi.js';
 import './style.scss';
+
+let body = '';
+movies.forEach((movie) => {
+  body += `<div class="card">
+        <img src="#" />
+        <a>${movie.name}</a>
+        <i class="fa-regular fa-heart"></i>
+        <p>5 likes</p>
+        <button id=${movie.id} data-target="#popup-wrapper">Comments</button>
+      </div>`;
+});
+
+const main = document.querySelector('main');
+main.innerHTML = body;
