4438265472e0e740d67c905054548bc50173b8e9
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-23 18:28:23 +0300

    finish homepage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import movies from './modules/TvShowApi.js';
+import movies from './modules/tvshowapi.js';
 import './style.scss';
 
 let body = '';
 movies.forEach((movie) => {
   body += `<div class="card">
-        <img src="#" />
-        <a>${movie.name}</a>
-        <i class="fa-regular fa-heart"></i>
+        <img class="card-banner" src="${movie.image.medium}" />
+        <div class="card-body">
+          <a class="movie-name">${movie.name}</a>
+          <a class="like-button" href="#"><i class="far fa-heart"></i></a>
+        </div>
         <p>5 likes</p>
-        <button id=${movie.id} data-target="#popup-wrapper">Comments</button>
+        <button class="comment-button" id=${movie.id} data-target="#popup-wrapper">Comments</button>
       </div>`;
 });
 
