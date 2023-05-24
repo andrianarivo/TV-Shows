7d2b371f51c43c0b40737820583e0e4c1ebea105
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-24 16:52:32 +0300

    build count item on index page

--- src/modules/DOMLoader.js
+++ src/modules/DOMLoader.js
@@ -3,5 +3,6 @@ const commentBtn = document.getElementById('submit-comment');
 const closeModal = document.getElementById('close-button');
 const username = document.getElementById('form-username');
 const commentBox = document.getElementById('comment-box');
+const logoTitle = document.querySelector('.logo-title');
 
-export { main, commentBtn, closeModal, username, commentBox };
+export { main, commentBtn, closeModal, username, commentBox, logoTitle };
