773f602e8ff0192efb54f5d88c9d0d145804e4e4
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-24 11:50:46 +0300

    ﻿build add like

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ const closeModal = document.getElementById('close-button');
 const main = document.querySelector('main');
 main.innerHTML = movieStore.render();
 
-document.addEventListener('click', (e) => {
+document.addEventListener('click', async (e) => {
   e.preventDefault();
 
   if (e.target.classList.contains('comment-button')) {
     renderModal(e);
     const movieIndex = e.target.id - 1;
     createModal(movieIndex, movieStore);
+  }
+  if (e.target.classList.contains('fa-heart')) {
+    const likes = await getLikes();
+    console.log('likes', likes);
   }
 });
 
--- src/modules/involvement_api.js
+++ src/modules/involvement_api.js
@@ -2,11 +2,16 @@ const appId = 'qK3z1QXp5g0btOAdCguF';
 const baseUrl =
   'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
 
-const getLikes = async () => {
+export const getLikes = async () => {
+  console.log('getlikes started..');
   const likes = await fetch(`${baseUrl}/${appId}/likes/`, { method: 'GET' });
-  debugger;
-  const likesJSON = await likes.json();
-  return likesJSON;
+  // debugger;
+  // const likesJSON = likes.json();
+  const likesText = likes.text();
+  // console.log(likesJSON);
+  console.log(likesText);
+
+  return likesText;
 };
 
-export { getLikes };
+// export default { getLikes };
